c4cb521b2d3fe927fa3413221327db1eadb4be21
Rifat <khadafy67@gmail.com>
2024-08-20 14:29:45 +0700

    update remote data source

--- besafe/Model/PlaceModel.swift
+++ besafe/Model/PlaceModel.swift
@@ -8,35 +8,18 @@
 import Foundation
 
 struct PlaceModel: Codable {
-    let businessStatus: String?
-    let icon: String?
-    let iconBackgroundColor: String?
-    let iconMaskBaseURI: String?
-    let name: String?
-    let openingHours: OpeningHours?
-    let placeID: String?
-    let types: [String]?
-    let userRatingsTotal: Int?
-    let vicinity: String?
-
-    enum CodingKeys: String, CodingKey {
-        case businessStatus = "business_status"
-        case icon
-        case iconBackgroundColor = "icon_background_color"
-        case iconMaskBaseURI = "icon_mask_base_uri"
-        case name
-        case openingHours = "opening_hours"
-        case placeID = "place_id"
-        case types
-        case userRatingsTotal = "user_ratings_total"
-        case vicinity
-    }
+    let id: String?
+    let location: Location?
+    let currentOpeningHours: CurrentOpeningHours?
+    let primaryType, shortFormattedAddress: String?
 }
 
-struct OpeningHours: Codable {
-    let openNow: Bool?
 
-    enum CodingKeys: String, CodingKey {
-        case openNow = "open_now"
-    }
+struct CurrentOpeningHours: Codable {
+    let openNow: Bool?
+}
+
+
+struct Location: Codable {
+    let latitude, longitude: Double?
 }
