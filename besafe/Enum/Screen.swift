bc4796613252d86290d1e3bc1806326852b5794f
Natasha <felicianhw@gmail.com>
2024-08-20 15:18:48 +0700

    merge emergency page view

--- besafe/Enum/Screen.swift
+++ besafe/Enum/Screen.swift
@@ -15,6 +15,7 @@ enum Screen: Identifiable, Hashable {
    case nearbyplacesview
    case emergencycontactsview
    case direction
+    case emergencypageview
    
    var id: Self { return self }
 }
